d741c5de47d37a191b2cf046be4d353af5daa767
kalamakadanca <kalamakadanca@seznam.cz>
2024-09-21 13:21:18 +0200

    Přidejte soubory projektu.

---
+++ uceni_wpf/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace uceni_wpf
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running...";
+                btnToggleRun.Content = "Stop";
+            }
+            
+            running = !running;
+        }
+    }
+}
